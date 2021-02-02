c4713975916fe24a19c8c62208433b5afadcc7bd
FUNNYYOU <47267524+Ammaridho@users.noreply.github.com>
2021-02-02 22:23:09 +0700

    Delete Re-Cloud directory

--- Re-Cloud/script.js
+++
@@ -1,9 +0,0 @@
-{/* <script>
-    var menu_btn = document.querySelector("#menu-btn")
-    var sidebar = document.querySelector("#sidebar")
-    var container = document.querySelector(".my-container")
-    menu_btn.addEventListener("click", () => {
-        sidebar.classList.toggle("active-nav")
-        container.classList.toggle("active-cont")
-    })
-</script> */}
