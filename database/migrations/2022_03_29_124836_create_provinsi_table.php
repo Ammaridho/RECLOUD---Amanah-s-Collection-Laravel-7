6d4b3c8fda47c948e2fae26a50a495461c2e5b78
ammaridho <47267524+Ammaridho@users.noreply.github.com>
2022-03-29 20:33:40 +0700

    menambahkan migration tapi belum di migrate, menghindari error

---
+++ database/migrations/2022_03_29_124836_create_provinsi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvinsiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinsi', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_provinsi',25);
+            $table->string('namaButton_provinsi',25);
+            $table->text('deskripsi_provinsi');
+            $table->string('gambar_provinsi',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinsi');
+    }
+}
